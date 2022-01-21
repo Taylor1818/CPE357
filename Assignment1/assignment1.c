b8217428d9d58a31142ffffa918f2f40e12a27ae
Taylor1818 <tbcmorgan@gmail.com>
2022-01-20 18:23:58 -0800

    Create assignment1.c

---
+++ Assignment1/assignment1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef unsigned short WORD;
+typedef unsigned int DWORD;
+typedef unsigned int LONG;
+
+typedef struct tagBITMAPFILEHEADER
+{
+    WORD bfType;  //specifies the file type
+    DWORD bfSize;  //specifies the size in bytes of the bitmap file
+    WORD bfReserved1;  //reserved; must be 0
+    WORD bfReserved2;  //reserved; must be 0
+    DWORD bfOffBits;  //specifies the offset in bytes from the bitmapfileheader to the bitmap bits
+}BITMAPFILEHEADER;
+
+typedef struct tagBITMAPINFOHEADER
+{
+    DWORD biSize;  //specifies the number of bytes required by the struct
+    LONG biWidth;  //specifies width in pixels
+    LONG biHeight;  //specifies height in pixels
+    WORD biPlanes;  //specifies the number of color planes, must be 1
+    WORD biBitCount;  //specifies the number of bits per pixel
+    DWORD biCompression;  //specifies the type of compression
+    DWORD biSizeImage;  //size of image in bytes
+    LONG biXPelsPerMeter;  //number of pixels per meter in x axis
+    LONG biYPelsPerMeter;  //number of pixels per meter in y axis
+    DWORD biClrUsed;  //number of colors used by the bitmap
+    DWORD biClrImportant;  //number of colors that are important
+}BITMAPINFOHEADER;
