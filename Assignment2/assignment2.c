18ac421e9d22bb12090e578bf41e8d57ff26fcb6
Taylor1818 <tbcmorgan@gmail.com>
2022-01-31 19:05:42 -0800

    Update assignment2.c

--- Assignment2/assignment2.c
+++ Assignment2/assignment2.c
@@ -7,7 +7,8 @@
 
 typedef unsigned char byte;
 
-struct chunkhead{
+struct chunkhead
+{
     unsigned int size;
     unsigned int info;
     struct chunkhead *next,*prev;
@@ -16,23 +17,21 @@ struct chunkhead{
 struct chunkhead *head;
 struct chunkhead *last;
 
-struct chunkhead* findBestFit(int size){
+struct chunkhead* findBestFit(int size)
+{
     struct chunkhead* addr = head;
     struct chunkhead* minAddr = 0;
-    while(1){
-        if(!addr->info){
-            if(addr->size == size){
+    while(1)
+    {
+        if(!addr->info)
+        {
+            if(addr->size == size)
+            {
                 return addr;
             } 
-            else if(addr->size > size){//valid space
-                if(minAddr == 0){
-                    minAddr = addr;
-                }
-                else if(minAddr->size > addr->size){
-                    minAddr = addr;
-                }
+            else if(addr->size > size && minAddr == 0 || minAddr->size > addr->size){//valid space
+                minAddr = addr;
             }
-            
         }
         if(!addr->next){
             last = addr;
@@ -47,41 +46,56 @@ struct chunkhead* findBestFit(int size){
     }
 }
 
-char* mymalloc(int size){
-    if(size<=0) return 0;   //handle bad inputs
+char* mymalloc(int size)
+{
+    if(size<=0)
+    {
+        return 0;
+    }
+    
     struct chunkhead *addr = head;
-    size+=24; //add 24 to size to handle chunk
-    int pages = 1+(int)(( (float)size / (float) PAGESIZE));
-    if(size % PAGESIZE) size = PAGESIZE*pages;      //only modify if not clean
-    if(!head){
+
+    size += sizeof(chunkhead);
+
+    int pages = 1 + size/PAGESIZE;
+
+    if(size % PAGESIZE)
+    {
+        size = PAGESIZE*pages; 
+    }
+
+    if(!head)
+    {
         printf("%c",0);
         head = sbrk(size);
-        head->size=size;     //sizeof(chunkhead);
-        head->info=1;           //empty head;
+        head->size=size;     
+        head->info=1;          
         head->next=0;head->prev=0;
         return (char *)(head +1);
     }
 
     struct chunkhead* best = findBestFit(size);
 
-    if(best){
+    if(best)
+    {
         addr = best;
         int sizeDif = addr->size - size;        
-        addr->size=size;                        //set new size of addr  
-        addr->info=1;                           //set chunk as occupied 
-        if(sizeDif){                            //handle splitting
-            int ima = ((char*)addr)+size-((char*)head);
-            
-            //struct chunkhead *newNext = (struct chunkhead*)&myheap[ima];
-            struct chunkhead *newNext = (struct chunkhead*)((char*)(addr)+size);
+        addr->size=size;                          
+        addr->info=1;                           
+
+        if(sizeDif)
+        {                            
+            struct chunkhead *newNext = (struct chunkhead*) addr+size;
             newNext->size = sizeDif;
             newNext->info = 0;
             newNext->prev = addr;
-            if(addr->next){
+
+            if(addr->next)
+            {
                 newNext->next = addr->next;
                 newNext->next->prev = newNext;
             }
-            addr->next = newNext;               //link to next
+            addr->next = newNext;               
         }
         return (char*)(addr+1);
     }
@@ -97,16 +111,21 @@ char* mymalloc(int size){
 
     addr->next=new;
 
-    return (char *)(new+1);
-
-    return 0;                                       //no available chunks
+    return (char *)(new+1);                                     
 }
 
-void myfree(char *address){
+void myfree(char *address)
+{
     struct chunkhead *addr = (struct chunkhead*)address;
-    addr-=1;
-    if(!address) return; 
-    if(addr == head && !head->next){
+    addr -= 1;
+
+    if(!address)
+    {
+        return; 
+    }
+
+    if(addr == head && !head->next)
+    {
         head = 0;
         brk(sbrk(0)-addr->size);
         return;
@@ -114,89 +133,131 @@ void myfree(char *address){
 
     addr->info = 0;
 
-    if(addr->prev && !addr->prev->info){  //if there is an empty previous
+    if(addr->prev && !addr->prev->info)
+    {
         addr->prev->size = addr->size+addr->prev->size;
         addr->prev->next = addr->next;
         if(addr->next) addr->next->prev = addr->prev;
         addr = addr->prev;
     }
-    if(addr->next){  //if there is an empty next
-        if(!addr->next->info){
+
+    if(addr->next)
+    {
+        if(!addr->next->info)
+        {
             addr->size = addr->size+addr->next->size;
             addr->next = addr->next->next;
-            if(addr->next) addr->next->prev = addr;
+
+            if(addr->next)
+            {
+                addr->next->prev = addr;
+            }
         }
     }
-    if(!addr->next){
-        if(addr == head){
+
+    if(!addr->next)
+    {
+        if(addr == head)
+        {
             head = 0;
-        } else {
+        } else
+        {
             addr->prev->next = 0;
         }
+
         brk(sbrk(0)-addr->size);
+
         return;
     }
 }
 
-void analyze(){
-    
+void analyze()
+{
     printf("\n--------------------------------------------------------------\n");
     
-    if(!head){
+    if(!head)
+    {
         printf("no heap\n");
         return;
     }
     struct chunkhead* ch = (struct chunkhead*)head;
 
-    for(int no=0; ch; ch = (struct chunkhead*)ch->next,no++){
+    for(int no = 0; ch; ch = (struct chunkhead*)ch->next, no++)
+    {
         printf("% 3d | current addr: %p |", no, ch);
         printf("size: % 10d | ", ch->size);
-        printf("%sinfo: %d\033[0m | ", ch-> info ? "\033[0;31m":"\033[0;32m", ch->info);
+        printf("info: %d | ", ch->info);
         printf("next: %p | ", ch->next);
-        printf("prev: %p", ch->prev);printf("      \n");
+        printf("prev: %p", ch->prev);
+        printf("      \n");
     }
 
     printf("program break on address: %p\n",sbrk(0));
 }
 
-void test(){
+void test()
+{
 
     byte* a[100];
 
-    analyze();//50% points
+    analyze();
 
-    for(int i=0;i<100;i++)a[i]= mymalloc(1000); 
-    for(int i=0;i<90;i++)myfree(a[i]);
+    for(int i=0;i<100;i++)
+    {
+        a[i]= mymalloc(1000);
+    }
 
-    analyze(); //50% of points if this is correct
+    for(int i=0;i<90;i++)
+    {
+        myfree(a[i]);
+    }
+
+    analyze();
 
     myfree(a[95]);
 
     a[95] = mymalloc(1000);
 
-    analyze();//25% points, this new chunk should fill the smaller free one 
+    analyze(); 
 
-    for(int i=90;i<100;i++)myfree(a[i]);
+    for(int i=90;i<100;i++)
+    {
+        myfree(a[i]);
+    }
 
-    analyze();// 25% should be an empty heap now with the start address
-    //from the program start
+    analyze();
 }
 
-void testTime(){
+void testTime()
+{
     byte* a[100];
+
     clock_t ca, cb;
     ca = clock();
-    for(int i=0;i<100;i++) a[i]= mymalloc(1000);
-    for(int i=0;i<90;i++) myfree(a[i]);
+
+    for(int i=0;i<100;i++){
+        a[i]= mymalloc(1000);
+    }
+    for(int i=0;i<90;i++){
+        myfree(a[i]);
+    } 
+
     myfree(a[95]);
+
     a[95] = mymalloc(1000);
-    for(int i=90;i<100;i++)myfree(a[i]);
+
+    for(int i=90;i<100;i++)
+    {
+        myfree(a[i]);
+    }
+
     cb = clock();
+
     printf("\nduration: %ld\n", (cb -ca));
 }
 
-void main(){
-    testTime();
+void main()
+{
     test();
-    
+    testTime();
 }
