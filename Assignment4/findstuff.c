ee69712e85edc0c367d6a0b27db9fbb9472da16f
Taylor <tbcmorgan@gmail.com>
2022-02-28 16:21:26 -0800

    Update findstuff.c

--- Assignment4/findstuff.c
+++ Assignment4/findstuff.c
@@ -19,16 +19,51 @@ typedef struct Child
     int status;
 } Child;
 
-void childProcess()
+int childAvail(Child *children)
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        if (children[i].pid == -1)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void findInner(Child *child, char *string)
 {
 }
 
-int find()
-{
-}
 
-int findSubDir()
+
+void findFileName(Child *child, char *string)
 {
+    char result[1024];
+
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(".");
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            if (dir->d_name[0] != '.')// check stat and if needing to be recursive
+            {
+                if(strcmp(dir->d_name, string) == 0)
+                {
+                    strcpy(result, dir-> d_name);
+                    fprintf(stderr, "%s", result);
+                    //strcat(result, '\0');
+                    //printf("%s%s\033[0m  ", dir->d_type != DT_DIR ? "\033[0m" : "\033[1;34m", dir->d_name);   
+                }
+                
+            }
+        }
+        closedir(d);
+    }
+    //write(child->fd[1], result, 1024);
+    printf("\n");
 }
 
 void quitting(Child *children)
@@ -89,22 +124,23 @@ void main()
         char *spaces = strtok(userInput, " ");
         char *strings[4];
 
-        while (spaces != NULL)
+        for (int i = 0; i < 4; i++)
         {
-            for (int i = 0; i < 4; i++)
+            if (spaces != NULL)
             {
                 strings[i] = spaces;
                 spaces = strtok(NULL, " ");
+                strings[i][strcspn(strings[i], "\n")] = '\0';
             }
         }
 
-        if (strcmp("q\n", strings[0]) == 0 || strcmp("quit\n", strings[0]) == 0)
+        if (strcmp("q", strings[0]) == 0 || strcmp("quit", strings[0]) == 0)
         {
             fprintf(stderr, "Quitting\n");
             quitting(children);
             break;
         }
-        else if (strcmp("list\n", strings[0]) == 0)
+        else if (strcmp("list", strings[0]) == 0)
         {
             list(children);
         }
@@ -114,7 +150,6 @@ void main()
             {
                 char temp[2];
                 sprintf(temp, "%d", i); // int to string
-                strcat(temp, "\n");     // add ending
 
                 if (strcmp(strings[1], temp) == 0)
                 {
@@ -124,11 +159,46 @@ void main()
         }
         else if (strcmp("find", strings[0]) == 0)
         {
-            char text[100] = &strings[1];
-            //text = *strings[1];
-            if (strcmp(strings[1], "text\n") == 0)
+
+            int cA = childAvail(children);
+
+            char text[1024];
+
+            strcpy(text, strings[1]);
+
+            if (text[0] == '"')
             {
-                printf("here");
+                int stringLength;
+                for (int i = 1; text[i] != '\0'; i++)
+                {
+                    if (text[i] == '"')
+                    {
+                        stringLength = i - 1;
+                        break;
+                    }
+                }
+                char *toSearchFor = text + 1;
+                stringLength[toSearchFor] = '\0';
+
+                int pid = fork();
+                if (pid == 0)
+                {
+                    findInner(children + cA, toSearchFor);
+                }
+            }
+            else //No quotations
+            {
+                int pid = fork();
+                if (pid == 0)
+                {
+                    (children+cA)->pid = getpid();
+                    findFileName(children + cA, text);
+
+                    char pipeRead[1024];
+                    //read((children + cA)->fd[0], pipeRead, 1024 );
+
+                    printf("%s", pipeRead);
+                }
             }
         }
     }
